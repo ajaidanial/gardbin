3f65b0548a2256e404f3060fc994e21311796cbc
ajaidanial <ajai@geopits.com>
2020-12-18 02:49:28 +0530

    added proper script for backup 2

--- start.sh
+++ start.sh
@@ -31,7 +31,7 @@ export PGPASSWORD="$db_password"
 
 # back up script visit https://gist.github.com/ajaidanial/91724d85bd899e3e4a905fa73a49f8b1 for more info
 pg_dump -F t -h "$db_host" -U "$db_user" "$db_database" >"$backup_file_name".backup
-gzip "$(backup_file_name)".backup
+gzip $(backup_file_name).backup
 
 # post back up meta
 unset PGPASSWORD
