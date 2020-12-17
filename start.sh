e2854b51aec24c9f0e9e590d02d332397a5b3b0d
ajaidanial <ajai@geopits.com>
2020-12-18 02:36:00 +0530

    added proper script for backup

--- start.sh
+++ start.sh
@@ -27,11 +27,11 @@ pip install -r requirements.txt
 echo "Setting up the backup..."
 
 mkdir backups
-export PGPASSWORD=${db_password}
+export PGPASSWORD="$db_password"
 
 # back up script visit https://gist.github.com/ajaidanial/91724d85bd899e3e4a905fa73a49f8b1 for more info
-pg_dump -F t -h "${db_host}" -U "${db_user}" "${db_database}" >"${backup_file_name}.backup"
-gzip "$(backup_file_name).backup"
+pg_dump -F t -h "$db_host" -U "$db_user" "$db_database" >"$backup_file_name".backup
+gzip "$(backup_file_name)".backup
 
 # post back up meta
 unset PGPASSWORD
